6316d6765610868b4fc5b554417fa2495ac71a4d
OliverG42 <ji20923@bristol.ac.uk>
2023-06-30 19:42:31 +0100

    Refactoring and speed
    - Refactored arm-related parts of the code into an ArmState object
    - Moved the regret and success rates calculation into the ArmState, and
    streamlined them
    - Moved all algorithms into the algorithms.py file
    - Added a profiler output to aid with optimising

---
+++ src/algorithms.py
@@ -0,0 +1,51 @@
+from random import randint
+
+import numpy as np
+
+
+# TODO Add "burn-in" time
+# TODO Try each arm x times, then switch to greedy
+def bernGreedy(arm_state):
+    return np.argmax(arm_state.success_rates)
+
+
+def bernTS(arm_state, alphaPrior=1, betaPrior=1):
+    samples = np.random.beta(
+        arm_state.successes + alphaPrior, arm_state.failures + betaPrior
+    )
+    return np.argmax(samples)
+
+
+def epsilonGreedy(arm_state, epsilon=0.9):
+    # Slowly decrease the probability to do something random
+    epsilon = epsilon * (0.9 ** arm_state.total_trials)
+
+    if np.random.random() < epsilon:
+        # Choose at random
+        chosen_arm = np.random.randint(arm_state.num_arms)
+    else:
+        # Choose greedily
+        chosen_arm = np.argmax(arm_state.success_rates)
+
+    return chosen_arm
+
+
+def completelyRandom(arm_state):
+    return randint(0, arm_state.num_arms - 1)
+
+
+def ucb(arm_state):
+    confidence_bounds = arm_state.success_rates + np.sqrt(
+        (2 * np.log(np.sum(arm_state.total_trials + 1)))
+        / np.where(arm_state.total_trials == 0, 1, arm_state.total_trials)
+    )
+
+    return np.argmax(confidence_bounds)
+
+
+#               Temperature
+# Exploitation <-----------> Exploration
+def softmax(arm_state, temperature=0.1):
+    exp_values = np.exp(arm_state.success_rates / temperature)
+    probabilities = exp_values / np.sum(exp_values)
+    return np.random.choice(len(arm_state.success_rates), p=probabilities)
--- src/main.py
+++ src/main.py
@@ -1,161 +1,100 @@
-from collections import namedtuple
-from random import randint
+from algorithms import bernTS, bernGreedy, completelyRandom, epsilonGreedy, ucb, softmax
 
+import cProfile, pstats
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import sem
-
-ArmState = namedtuple("ArmState", ["successes", "failures", "num_arms"])
 
 
-def getSuccessRates(arm_state, divisionByZeroIs=0):
-    total_trials = arm_state.successes + arm_state.failures
+class ArmState:
+    def __init__(self, reward_probs):
+        self.reward_probs = reward_probs
+        self.max_prob = np.max(reward_probs)
+        self.num_arms = len(reward_probs)
+        self.successes = np.zeros(self.num_arms)
+        self.failures = np.zeros(self.num_arms)
+        self.total_trials = 0
+        self.success_rates = np.ones(self.num_arms)
+        self.regrets = []
 
-    success_rates = arm_state.successes / total_trials.size
+    def pull_arm(self, arm_number):
+        outcome = np.random.binomial(1, self.reward_probs[arm_number])
+        if outcome:
+            self.successes[arm_number] += 1
+        else:
+            self.failures[arm_number] += 1
 
-    # Force arms that haven't been tried yet to have a success rate of 1
-    success_rates[(success_rates == 0.0) & (total_trials == 0)] = divisionByZeroIs
+        self.total_trials += 1
 
-    return success_rates
+        # Update success rates
+        self.success_rates[arm_number] = self.successes[arm_number] / self.total_trials
 
-
-def bernNearlyGreedy(arm_state):
-    # Avoids messy division by zero warning
-    theta = getSuccessRates(arm_state, divisionByZeroIs=1)
-    return np.argmax(theta)
-
-
-def bernGreedy(arm_state):
-    # Avoids messy division by zero warning
-    theta = getSuccessRates(arm_state)
-    return np.argmax(theta)
-
-
-def bernTS(arm_state):
-    samples = np.random.beta(arm_state.successes + 1, arm_state.failures + 1)
-    return np.argmax(samples)
-
-
-def epsilonGreedy(arm_state, epsilon=0.1):
-    if np.random.random() < epsilon:
-        # Choose at random
-        chosen_arm = np.random.randint(arm_state.num_arms)
-    else:
-        # Choose greedily
-        success_rates = getSuccessRates(arm_state, divisionByZeroIs=0)
-        chosen_arm = np.argmax(success_rates)
-
-    return chosen_arm
-
-
-def completelyRandom(arm_state):
-    return randint(0, arm_state.num_arms - 1)
-
-
-def ucb(arm_state):
-    total_trials = arm_state.successes + arm_state.failures
-    success_rates = getSuccessRates(arm_state, divisionByZeroIs=1)
-
-    confidence_bounds = success_rates + np.sqrt(
-        (2 * np.log(np.sum(total_trials + 1))) / np.where(total_trials == 0, 1, total_trials)
-    )
-
-    return np.argmax(confidence_bounds)
-
-
-#               Temperature
-# Exploitation <-----------> Exploration
-def softmax(arm_state, temperature=0.1):
-    success_rates = getSuccessRates(arm_state, divisionByZeroIs=1)
-
-    exp_values = np.exp(success_rates / temperature)
-    probabilities = exp_values / np.sum(exp_values)
-    return np.random.choice(len(success_rates), p=probabilities)
-
-
-def calculate_regret(reward_probs, chosen_arm):
-    max_reward_prob = np.max(reward_probs)
-    chosen_reward_prob = reward_probs[chosen_arm]
-    regret = max_reward_prob - chosen_reward_prob
-    return regret
+        # Update regret
+        self.regrets.append(self.max_prob - self.reward_probs[arm_number])
 
 
 def perform_sampling(choosing_function, reward_probs, num_trials):
-    num_arms = len(reward_probs)
-    arm_state = ArmState(
-        successes=np.zeros(num_arms), failures=np.zeros(num_arms), num_arms=num_arms
-    )
-    allRegrets = []
+    arm_state = ArmState(reward_probs)
 
     for _ in range(num_trials):
         chosen_arm = choosing_function(arm_state)
-        reward = np.random.binomial(1, reward_probs[chosen_arm])
+        arm_state.pull_arm(chosen_arm)
 
-        if reward:
-            arm_state.successes[chosen_arm] += 1
-        else:
-            arm_state.failures[chosen_arm] += 1
-
-        regret = calculate_regret(reward_probs, chosen_arm)
-        allRegrets.append(regret)
-
-    return allRegrets
+    return arm_state.regrets
 
 
 def makeGraph(reward_probs):
     print(reward_probs)
-    num_trials = 100  # How many "lever pulls" are there?
-    num_samples = 100  # How many "runs" are there?
-
-    num_trials += 1
-    errorBarInterval = int(num_trials / 10)
-
     plt.figure(figsize=(10, 6))
 
     functions = [
         bernTS,
         bernGreedy,
-        bernNearlyGreedy,
-        completelyRandom,
+        # completelyRandom,
         epsilonGreedy,
         ucb,
         softmax,
     ]
-    colours = ["red", "yellow", "green", "blue", "purple", "pink", "brown"]
+    colours = ["red", "yellow", "green", "blue", "purple", "pink"]
+
+    num_trials = 100 + 1  # How many "lever pulls" are there?
+    num_samples = 100  # How many "runs" are there?
+    errorBarInterval = int(num_trials / 10)
 
     for choosing_function, colour in zip(functions, colours):
         print("Computing for", choosing_function.__name__)
-        all_regrets = []
-        for _ in range(num_samples):
-            regrets = perform_sampling(choosing_function, reward_probs, num_trials)
-            cumulative_regrets = np.cumsum(regrets)
-            all_regrets.append(cumulative_regrets)
 
-            # Plotting the cumulative regret with adjusted line width
-            plt.plot(
-                range(1, num_trials + 1),
-                cumulative_regrets,
-                alpha=0.05,
-                linewidth=0.8,
-                color=colour,
-            )
+        all_cumulative_regrets = [
+            np.cumsum(perform_sampling(choosing_function, reward_probs, num_trials))
+            for _ in range(num_samples)
+        ]
 
-        avg_regrets = np.mean(all_regrets, axis=0)
+        avg_cumulative_regrets = np.mean(all_cumulative_regrets, axis=0)
+        std_cumulative_regrets = np.std(avg_cumulative_regrets)
 
-        # Plot the smooth curve of average cumulative regrets
-        plt.plot(
+        # Plot the smooth curve of average cumulative regrets with error bars
+        plt.errorbar(
             range(1, num_trials + 1, errorBarInterval),
-            avg_regrets[::errorBarInterval],
+            avg_cumulative_regrets[::errorBarInterval],
+            yerr=std_cumulative_regrets,
             label=f"{choosing_function.__name__}",
             linewidth=2,
             color=colour,
+            alpha=0.8,
         )
 
-    plt.xlabel("Trials" f"{reward_probs}")
+    plt.xlabel(f"Trials {reward_probs}")
     plt.ylabel("Cumulative Regret")
     plt.legend()
 
 
 if __name__ == "__main__":
-    makeGraph([0.6, 0.65, 0.7])
+    profiler = cProfile.Profile()
+    profiler.enable()
+    makeGraph([x / 100 for x in range(0, 100)])
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats("tottime")
+    stats.print_stats(10)
+    stats = pstats.Stats(profiler).sort_stats("cumtime")
+    stats.print_stats(10)
     plt.show()
+    # plt.savefig("graph.png")
